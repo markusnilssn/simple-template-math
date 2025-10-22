a6c68ea1ed3ee5b17da5febcbee6c91f23799660
Markus Nilsson <markusjnilsson@hotmail.com>
2025-10-22 12:10:56 +0200

    remove trash

--- build/CMakeFiles/Application.dir/src/Math.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Application.dir/src/Math.cpp.o: \
-  /Users/markus/Documents/GitHub/simple-template-math/src/Math.cpp \
-  /Users/markus/Documents/GitHub/simple-template-math/include/Math.hpp
